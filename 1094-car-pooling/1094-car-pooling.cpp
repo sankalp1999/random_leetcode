409b6233342fc81562e2a8f8c90b4d98fcc03726
Sankalp Shubham <45618047+sankalp1999@users.noreply.github.com>
2022-03-29 16:09:12 +0530

    Time: 7 ms (99.21%), Space: 10.7 MB (37.33%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     
-    static bool compare(vector<int>& a, vector<int>& b)
-    {
-        if(a[0]==b[0])
-        {
-            return a[1]>b[1];
-        }
-        return a[0]<b[0];
-    }
+    
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<vector<int>> v;
-        for(int i = 0; i < trips.size(); i++)
-        {
-            vector<int> temp = {trips[i][1],1,trips[i][0] };
-            v.push_back(temp);
-            vector<int> temp2 = {trips[i][2],2,trips[i][0]} ;
-            v.push_back(temp2);
-        }
-        sort(v.begin(), v.end(),compare);
         int curr = 0;
-        for(int i = 0; i < v.size(); i++)
+        vector<int> stops(1002, 0);
+        for(auto trip : trips)
         {
-            if(v[i][1]==1)curr+=v[i][2];
-            if(v[i][1]==2)curr-=v[i][2];
-            if(curr>capacity)return false;
+            stops[trip[1]] += trip[0];
+            stops[trip[2]] -= trip[0]; // At a particular time, how many peeps get out
+        }
+        for(int i = 0; i < 1001; i++)
+        {
+            curr += stops[i];
+            if(curr > capacity)return false;
         }
         return true;
     }
